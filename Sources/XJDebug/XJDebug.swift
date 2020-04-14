750b934900dbed0d0a2eda6340098141ffbaaf3a
liuxiangjing <lxj_tintin@126.com>
2020-04-14 12:55:00 +0800

    添加代码

--- Sources/XJDebug/XJDebug.swift
+++ Sources/XJDebug/XJDebug.swift
@@ -1,7 +1,24 @@
-public struct XJDebug {
-    public var text = "Hello, World!"
-    public mutating func testLog() {
-        text = "Hello J!"
-        print(text)
+
+#if !os(macOS)
+fileprivate let debugViewTag = 816
+import UIKit
+open class XJDebug {
+    open static func showDebugView() {
+        let debugView = XJDebugView(frame: CGRect(x: 0, y: 200, width: 100, height: 105))
+        debugView.tag = debugViewTag
+        
+        if let keyWindow = UIApplication.shared.windows.first {
+            debugView.backgroundColor = UIColor.lightGray
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                keyWindow.addSubview(debugView)
+                debugView.run()
+            }
+        }
+    }
+    open static func closeDebugView() {
+        if let keyWindow = UIApplication.shared.windows.first {
+                   keyWindow.viewWithTag(debugViewTag)?.removeFromSuperview()
+               }
     }
 }
+#endif
