9b1c32701b21ad77ea4a6a62f46e417ba3e7138a
liuxiangjing <lxj_tintin@126.com>
2020-04-16 17:56:40 +0800

    测试

--- Sources/XJDebug.swift
+++ Sources/XJDebug.swift
@@ -7,38 +7,29 @@
 //
 //
 
+#if os(iOS)
+import UIKit
+fileprivate let debugViewTag = 816
+public class XJDebug {
+    public func `init`() {}
 
-public struct XJDebug {
-    public init() {}
-    public var text = "Hello, World!"
-    public func printText() {
-        print(self.text)
+    public static func showDebugView() {
+        let debugView = XJDebugView(frame: CGRect(x: 0, y: 200, width: 100, height: 105))
+        debugView.tag = debugViewTag
+
+        if let keyWindow = UIApplication.shared.windows.first {
+            debugView.backgroundColor = UIColor.lightGray
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                keyWindow.addSubview(debugView)
+                debugView.run()
+            }
+        }
     }
-
+    public static func closeDebugView() {
+        if let keyWindow = UIApplication.shared.windows.first {
+            keyWindow.viewWithTag(debugViewTag)?.removeFromSuperview()
+        }
+    }
 }
-//#if os(iOS)
-//import UIKit
-//fileprivate let debugViewTag = 816
-//
-//open class XJDebug {
-//
-//    public static func showDebugView() {
-//        let debugView = XJDebugView(frame: CGRect(x: 0, y: 200, width: 100, height: 105))
-//        debugView.tag = debugViewTag
-//
-//        if let keyWindow = UIApplication.shared.windows.first {
-//            debugView.backgroundColor = UIColor.lightGray
-//            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-//                keyWindow.addSubview(debugView)
-//                debugView.run()
-//            }
-//        }
-//    }
-//    public static func closeDebugView() {
-//        if let keyWindow = UIApplication.shared.windows.first {
-//            keyWindow.viewWithTag(debugViewTag)?.removeFromSuperview()
-//        }
-//    }
-//}
-//
-//#endif
+
+#endif
